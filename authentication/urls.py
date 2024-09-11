82c8b01eadd5eb0fafb13400a1c438ac0e6b8eba
KhanAfsar12 <ka484564@gmail.com>
2024-09-11 16:28:09 +0530

    forget api created

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect
 from django.urls import path
-from .views import  register, user_login, user_logout
+from .views import  register, resetPassword, user_forget, user_login, user_logout
 from django.contrib.auth.views import  LogoutView
 from django.contrib.auth import logout
 
@@ -14,4 +14,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('user_forget/', user_forget, name='user_forget'),
+    path('resetPassword/', resetPassword, name='resetPassword'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, ForgetForm, ResetPasswordForm
 from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.contrib.auth import logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
@@ -52,3 +53,56 @@ def user_logout(request):
         return JsonResponse({'message': 'Successfully logged out'})
     
     return redirect('login')
+
+
+
+def user_forget(request):
+    if request.method == 'POST':
+        form = ForgetForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data['username']
+            user = User.objects.filter(username=data).first()
+            request.session['user'] = user.username
+            if user:
+                return redirect('resetPassword') 
+            else:
+                messages.error(request, "User not found.")
+                return redirect('user_forget')
+        else:
+            messages.error(request, "There was an issue with the form.")
+    else:
+        form = ForgetForm()
+
+    return render(request, 'user_forget.html', {'form': form})
+
+
+
+
+def resetPassword(request):
+    if request.method == 'POST':
+        form = ResetPasswordForm(request.POST)
+        if form.is_valid():
+            password1 = form.cleaned_data.get('password1')
+            password2 = form.cleaned_data.get('password2')
+
+            if password1 != password2:
+                return HttpResponse('Password must be same')
+            
+            username = request.session.get('user') # Only user name will be stored in user variable
+            if not username:
+                return redirect('user_forget')
+            
+            user = User.objects.filter(username=username).first()
+            if not user:
+                messages.warning(request, f'User {username} is not found!')
+                return redirect('register')
+            
+            user.set_password(password1)
+            user.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"{username}, You are successfully reset your password.")
+            return redirect('login')
+
+    else:
+        form = ResetPasswordForm()
+    return render(request, 'reset_password.html', {'form': form})
