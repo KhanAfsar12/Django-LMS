ac0b82e1020670112d59debaac836795ac06635f
KhanAfsar12 <ka484564@gmail.com>
2024-11-06 10:02:46 +0530

    Improve the layout of course section

--- LMS/urls.py
+++ LMS/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('dj-admin/', admin.site.urls),
     path('', include('course_manage.urls')),
     path('authentication/', include('authentication.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
